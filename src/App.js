92d18da7a10e33a9a0926bf443fdb785e26bf6c9
Krushinsky <tj_chel@mail.ru>
2022-07-23 18:20:40 +0500

    upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { data } from './data'
 
 function App() {
+  const [film, setFilm] = useState (0)
+  const{id, title, description, image} = data[film]
+  const previousTitle = () =>{
+    setFilm(film =>{
+      film--;
+      if (film < 0){
+        return data.length -1
+      }
+      return film
+    })
+  }
+  const nextTitle = () =>{
+    setFilm(film =>{
+      film++;
+      if (film > data.length-1){
+        film = 0
+      }
+      return film;
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <h1>Mультфильмы</h1>
+      </div>
+      
+          <div className='slide'>
+            <div>
+              <button onClick={previousTitle}>❮</button>
+            </div>
+
+            <div>
+              <h2>{title}</h2>
+              <img src={image} width = '300px'/>
+            </div>
+    
+            <div>
+              <button onClick={nextTitle}>❯</button>
+            </div>
+          </div>
+    
+      <div>
+      <input/>
+      </div>
+        
+        
+      
+      <div>
+        <button>CCCP</button>
+        <button>Российские</button>
+        <button>Disney</button>
+        <button>Illumination</button>
+        <button>Pixar</button>
+      </div>
+      <div>
+        <h3>
+          
+        </h3>
+      </div>
+      <div>
+              <h2>{title}</h2>
+              <img src={image} width = '300px'/>
+            </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,209 @@
+import dog from './dog.jpg';
+import fly from './fly.jpg';
+import hedgehog from './hedgehog.jpg'
+import mother from './mother.jpg'
+import vovka from './vovka.jpg'
+import alesha from './alesha.jpg'
+import fix from './fix.jpeg'
+import knyaz from './knyaz.webp'
+import krepost from './krepost.jpg'
+import snow from './snow.jpg'
+import golovolomka from './golovolomka.webp'
+import monsters from './monsters.webp'
+import ratatouille from './ratatouille.webp'
+import soul from './soul.webp'
+import walle from './walle.webp'
+import lion from './lion.webp'
+import mermaid from './mermaid.webp'
+import aladdin from './aladdin.webp'
+import zoo from './zoo.webp'
+import beast from './beast.webp'
+import grinch from './grinch.jpg'
+import iam from './iam.jpg'
+import sing from './sing.webp'
+import singTwo from './sing2.jpg'
+import secretlife from './secretlife.webp'
+
+
+
+
+
+export const data = [
+    {
+    id: 1,
+    title:'Жил-был пёс',
+    description: "СССР, 1982 год. Комедия. Длительность: 10 минут.",
+    image: dog
+    },
+
+    {
+        id: 2,
+        title:'Летучий корабль',
+        description: "СССР, 1979 год. Сказка, музыкальный. Длительность: 18 минут",
+        image: fly    
+    },
+
+    {
+        id: 3,
+        title:'Ёжик в тумане',
+        description: "СССР, 1975 год. Сказка, притча. Длительность: 10 минут.",
+        image: hedgehog    
+    },
+
+    {
+        id: 4,
+        title:'Мама для мамонтёнка',
+        description: "СССР, 1981 год. Сказка, приключения. Длительность: 8 минут.",
+        image: mother    
+    },
+
+    {
+        id: 5,
+        title:'Вовка в Тридевятом царстве',
+        description: "СССР, 1965 год. Сказка, приключения. Длительность: 19 минут.",
+        image: vovka    
+    },
+    //
+    {
+        id: 6,
+        title:'Алеша Попович и Тугарин Змей',
+        description: "Год: 2004, Страна: Россия, Хронометраж: 1 час 19 минут",
+        image: alesha    
+    },
+
+    {
+        id: 7,
+        title:'Крепость: Щитом и мечом',
+        description: "Год: 2015, Страна: Россия, Хронометраж: 1 час 32 минуты",
+        image: krepost    
+    },
+
+    {
+        id: 8,
+        title:'Снежная королева: Зазеркалье',
+        description: "Год: 2018, Страна: Россия, Хронометраж: 1 час 20 минут",
+        image: snow    
+    },
+
+    {
+        id: 9,
+        title:'Фиксики: Большой секрет',
+        description: "Год 2017, Страна: Россия, Хронометраж: 1 час 20 минут",
+        image: fix    
+    },
+
+    {
+        id: 10,
+        title:'Князь Владимир',
+        description: "Год 2004, Страна: Россия, Хронометраж: 1 час 18 минут",
+        image: knyaz    
+    },
+    //
+    {
+        id: 11,
+        title:'Душа',
+        description: "Год 2020, Страна: США, Хронометраж: 1 час 46 минут",
+        image: soul    
+    },
+
+    {
+        id: 12,
+        title:'Рататуй',
+        description: "Год 2007, Страна: США, Хронометраж: 1 час 51 минут",
+        image: ratatouille    
+    },
+
+    {
+        id: 13,
+        title:'Головоломка',
+        description: "Год 2015, Страна: США, Хронометраж: 1 час 35 минут",
+        image: golovolomka    
+    },
+
+    {
+        id: 14,
+        title:'Корпорация монстров',
+        description: "Год 2001, Страна: США, Хронометраж: 1 час 32 минут",
+        image: monsters    
+    },
+
+    {
+        id: 15,
+        title:'ВАЛЛ·И',
+        description: "Год 2017, Страна: США, Хронометраж: 1 час 38 минут",
+        image: walle    
+    },
+    //
+    {
+        id: 16,
+        title:'Аладдин',
+        description: "Год 1992, Страна: США, Хронометраж: 1 час 30 минут",
+        image: aladdin    
+    },
+    
+    {
+        id: 17,
+        title:'Русалочка',
+        description: "Год 1989, Страна: США, Хронометраж: 1 час 23 минут",
+        image: mermaid    
+    },
+
+    {
+        id: 18,
+        title:'Красавица и чудовище',
+        description: "Год 1991, Страна: США, Хронометраж: 1 час 24 минут",
+        image: beast    
+    },
+
+    {
+        id: 19,
+        title:'Зверополис',
+        description: "Год 2016, Страна: США, Хронометраж: 1 час 48 минут",
+        image: zoo    
+    },
+
+    {
+        id: 20,
+        title:'Король Лев',
+        description: "Год 1994, Страна: США, Хронометраж: 1 час 28 минут",
+        image: lion    
+    },
+    //
+    {
+        id: 21,
+        title:'Тайная жизнь домашних животных ',
+        description: "Год 2016, Страна: США, Франция, Япония, Хронометраж: 1 час 27 минут",
+        image: secretlife    
+    },
+
+    {
+        id: 22,
+        title:'Зверопой 2',
+        description: "Год 2021, Страна: США, Япония, Хронометраж: 1 час 50 минут",
+        image: singTwo    
+    },
+
+    {
+        id: 23,
+        title:'Зверопой',
+        description: "Год 2016, Страна: США, Япония, Хронометраж: 1 час 48 минут",
+        image: sing    
+    },
+
+    {
+        id: 24,
+        title:'Гадкий я',
+        description: "Год 2010, Страна: США, Франция, Япония, Хронометраж: 1 час 35 минут",
+        image: iam    
+    },
+
+    {
+        id: 25,
+        title:'Гринч',
+        description: "Год 2018, Страна: США, Франция, Япония, Хронометраж: 1 час 25 минут",
+        image: grinch    
+    }
+
+
+
+]
